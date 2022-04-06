29b891c4776bed43acfd739829f5df282f8b293e
Rick A. Crist <rickcrist1@msn.com>
2022-04-06 12:04:08 -0600

    Test diagram with empty menu bar

--- TestDiagram/TestDigramMainForm.Designer.cs
+++ TestDiagram/TestDigramMainForm.Designer.cs
@@ -29,13 +29,49 @@ namespace TestDiagram
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelMainMenu = new System.Windows.Forms.Panel();
+            this.schematicCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.schematicCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMainMenu
+            // 
+            this.panelMainMenu.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panelMainMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMainMenu.Name = "panelMainMenu";
+            this.panelMainMenu.Size = new System.Drawing.Size(800, 52);
+            this.panelMainMenu.TabIndex = 0;
+            // 
+            // schematicCanvas
+            // 
+            this.schematicCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.schematicCanvas.Location = new System.Drawing.Point(0, 52);
+            this.schematicCanvas.Name = "schematicCanvas";
+            this.schematicCanvas.Size = new System.Drawing.Size(800, 398);
+            this.schematicCanvas.TabIndex = 1;
+            this.schematicCanvas.TabStop = false;
+            // 
+            // TestDigramMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.schematicCanvas);
+            this.Controls.Add(this.panelMainMenu);
+            this.Name = "TestDigramMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test Diagram";
+            ((System.ComponentModel.ISupportInitialize)(this.schematicCanvas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMainMenu;
+        private System.Windows.Forms.PictureBox schematicCanvas;
     }
 }
 
