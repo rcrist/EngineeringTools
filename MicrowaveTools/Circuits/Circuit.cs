57eb54ec396036b8112558bdffda567f4567b1ff
Rick A. Crist <rickcrist1@msn.com>
2022-04-14 16:41:36 -0600

    Microwave Tools Circuit Class

---
+++ MicrowaveTools/Circuits/Circuit.cs
@@ -0,0 +1,14 @@
+﻿// C# Libraries
+using System;
+using System.Collections.Generic;
+
+// Microwave Tools Libraries
+using MicrowaveTools.Components;
+
+namespace MicrowaveTools.Circuits
+{
+    public class Circuit
+    {
+        public List<Comp> comps = new List<Comp>();
+    }
+}
---
+++ MicrowaveTools/Components/Comp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MicrowaveTools.Components
+{
+    public class Comp
+    {
+
+    }
+}
