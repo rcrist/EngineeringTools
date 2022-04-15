fc1ec31141cec8b1055406e1b96fe945a3363d79
Rick A. Crist <rickcrist1@msn.com>
2022-04-15 05:31:41 -0600

    Microwave Tools component base class

--- MicrowaveTools/Components/Comp.cs
+++ MicrowaveTools/Components/Comp.cs
@@ -1,10 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace MicrowaveTools.Components
 {
     public class Comp
     {
+        // Protected property variables for Property Grid
+        protected String _orientation = "Series";
+        protected String _type;
+        protected String _name;
+        protected float _value;
+        protected Point _loc;
+        protected int[] _nodes;
+        protected int _width;
+        protected int _height;
 
+        // Orientation property with category attribute and description attribute added   
+        [CategoryAttribute("Location Settings"), DescriptionAttribute("Orientation of the Component")]
+        public string Orientation
+        {
+            get
+            {
+                return _orientation;
+            }
+            set
+            {
+                _orientation = value;
+            }
+        }
+
+        // Location property with category attribute and description attribute added   
+        [CategoryAttribute("Location Settings"), DescriptionAttribute("Orientation of the Component")]
+        public Point Loc
+        {
+            get
+            {
+                return _loc;
+            }
+            set
+            {
+                _loc = value;
+            }
+        }
+
+        // Width property with category attribute and description attribute added   
+        [CategoryAttribute("Location Settings"), DescriptionAttribute("Orientation of the Component")]
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+
+        // Location property with category attribute and description attribute added   
+        [CategoryAttribute("Location Settings"), DescriptionAttribute("Orientation of the Component")]
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = value;
+            }
+        }
+
+        // Value property with category attribute and description attribute added   
+        [CategoryAttribute("Configuration Settings"), DescriptionAttribute("Configuration of the Component")]
+        public float Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        // Type property with category attribute and description attribute added   
+        [CategoryAttribute("Configuration Settings"), DescriptionAttribute("Configuration of the Component")]
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+
+        // Type property with category attribute and description attribute added   
+        [CategoryAttribute("Configuration Settings"), DescriptionAttribute("Configuration of the Component")]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        // Type property with category attribute and description attribute added   
+        [CategoryAttribute("Configuration Settings"), DescriptionAttribute("Configuration of the Component")]
+        public int[] Nodes
+        {
+            get
+            {
+                return _nodes;
+            }
+            set
+            {
+                _nodes = value;
+            }
+        }
+
+        // Polymorphism: Virtual methods used in circuit component iteration
+        public virtual void print() { /* Do noting */ }
+        public virtual void Draw(Graphics gr) { /* Do noting */ }
     }
 }
